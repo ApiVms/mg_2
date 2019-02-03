232f56bc5d63741d7be5efd90da994d82eecf91e
ApiVms <apivms@gmail.com>
2019-02-03 15:17:28 +0200

    1.2

---
+++ app/code/Training/Training_AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Training\Training_AdditionalTestOM\Model;
+
+use Training\Training_TestOM\Model\ManagerInterface;
+
+class ManagerCustom implements ManagerInterface {
+    public function create() {
+        
+    }
+    public function get() {
+        
+    }
+}
