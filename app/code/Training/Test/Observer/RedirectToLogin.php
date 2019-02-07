264a802659d8d716ebf83c651899683a66bb2c28
ApiVms <apivms@gmail.com>
2019-02-07 21:43:29 +0200

    1.10 (edited)

--- app/code/Training/Test/Observer/RedirectToLogin.php
+++ app/code/Training/Test/Observer/RedirectToLogin.php
@@ -14,34 +14,34 @@ class RedirectToLogin implements ObserverInterface {
      * @var \Magento\Fremework\App\ActionFlag $actionFlag
      */
     private $actionFlag;
+    /**
+     * @var \Magento\Customer\Model\Session $customerSession
+     */
+    protected $customerSession;
     
     /**
     * @param \Magento\Framework\App\Response\RedirectInterface $redirect
     * @param \Magento\Framework\App\ActionFlag $actionFlag
     */
     public function __construct(
-        \Magento\Framework\App\Response\RedirectInterface $redirect
-        //\Magento\Fremework\App\ActionFlag $actionFlag
+        \Magento\Framework\App\Response\RedirectInterface $redirect,
+        \Magento\Framework\App\ActionFlag $actionFlag,
+        \Magento\Customer\Model\Session $customerSession
     ) {
         $this->redirect = $redirect;
-        //$this->actionFlag = $actionFlag;
+        $this->actionFlag = $actionFlag;
+        $this->customerSession = $customerSession;
     }
     
     public function execute(\Magento\Framework\Event\Observer $observer) {
         
         $request = $observer->getEvent()->getData('request');
         
-        // exit();
-        
-        if ($request->getFullActionName() == 'catalog_product_view' // altenative way
-//        if (
-//               $request->getModuleName() == 'catalog'
-//            && $request->getControllerName() == 'product'
-//            && $request->getActionName() == 'view'
-        ) {
+        if ( !$this->customerSession->isLoggedIn() ) {
             $controller = $observer->getEvent()->getData('controller_action');
-            //$this->actionFlag->set('', \Magento\Framework\App\Action\Action::FLAG_NO_DISPATCH, true);
+            $this->actionFlag->set('', \Magento\Framework\App\Action\Action::FLAG_NO_DISPATCH, true);
             $this->redirect->redirect($controller->getResponse(), 'customer/account/login');
         }
+
     }    
 }
