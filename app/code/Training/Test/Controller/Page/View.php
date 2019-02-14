e3539e3bdbed5631f7f6f2842dcbb466fb551d5c
ApiVms <apivms@gmail.com>
2019-02-14 22:12:42 +0200

    2.6

--- app/code/Training/Test/Controller/Page/View.php
+++ app/code/Training/Test/Controller/Page/View.php
@@ -6,45 +6,33 @@ use \Magento\Framework\Exception\NoSuchEntityException;
 
 class View extends \Magento\Cms\Controller\Page\View {
     
-    protected $resultJsonFactory;
-    protected $pageRepository;
-    
+    protected $customerSession;
+    protected $context;
+    protected $resultForwardFactory;
+
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Controller\Result\ForwardFactory $resultForwardFactory,
-        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
-        \Magento\Cms\Api\PageRepositoryInterface $pageRepository
+        \Magento\Customer\Model\Session $customerSession
     ) {
-        parent::__construct($context, $resultForwardFactory);
+        $this->customerSession = $customerSession;
+        $this->resultForwardFactory = $resultForwardFactory;
+        $this->context = $context;
+        $this->resultForwardFactory = $resultForwardFactory;
         
-        $this->resultJsonFactory = $resultJsonFactory;
-        $this->pageRepository = $pageRepository;
-       
+        parent::__construct($this->context, $this->resultForwardFactory);
     }
     
     public function execute() {
         
-        if ($this->getRequest()->isAjax()) {
-            $data = ['status' => 'success', 'message' => ''];
-            // $pageId = $this->getRequest()->getParam('page_id', $this->getRequest()->getParam('id', false));
-            $pageId = $this->getRequest()->getParam('page_id', false);
-            $resultJson = $this->resultJsonFactory->create();
-            try {
-                $page = $this->pageRepository->getById($pageId);
-                $data['title'] = $page->getTitle();
-                $data['content'] = $page->getContent();
-            } catch (NoSuchEntityException $e) {
-                $data['status'] = 'error';
-                $data['message'] = 'Not found';
-            } catch (\Exception $e) {
-                $data['status'] = 'error';
-                $data['message'] = 'Something wrong';
-            }
-            $resultJson->setData($data);
-            
-            return $resultJson;
+        if ( !$this->customerSession->isLoggedIn() ) {
+            $forwardResult = $this->resultForwardFactory->create();
+            $forwardResult->setModule('customer');
+            $forwardResult->setController('account');
+            $forwardResult->forward('login');
+            return $forwardResult;
         }
-
         return parent::execute();
     }
 }
